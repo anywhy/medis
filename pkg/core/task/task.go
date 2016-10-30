7271a037e2617de1ea13c7b4b361dddc1a473553
markyang-_- <yangdaixing168@163.com>
2016-10-30 17:08:12 +0800

    update

--- pkg/core/task/task.go
+++ pkg/core/task/task.go
@@ -3,12 +3,18 @@ package task
 import mesos "github.com/mesos/mesos-go/mesosproto"
 
 type Task struct {
-	InstanceId string         `json:"instanceId"`
-	Id         string         `json:"id"`
-	Type       string         `json:"type"`
-	Stauts     string         `json:"status"`
-	Ip         string         `json:"ip"`
-	Port       int            `json:"port"`
-	TaskInfo   mesos.TaskInfo `json:"taskInfo"`
-	Dependent  Task           `json:"dependent"`
+	InstanceId string          `json:"instanceId"`
+	Id         string          `json:"id"`
+	Name       string          `json:"name"`
+	Cpus       float64         `json:"cpus"`
+	Mem        float64         `json:"mem"`
+	Disk       float64         `json:"disk"`
+	Command    string          `json:"cmd"`
+	Volumes    []*mesos.Volume `json:"volumes,omitempty"`
+	Ports      []*mesos.Port   `json:"port_mappings,omitempty"`
+	TaskInfo   *mesos.TaskInfo `json:"taskInfo"`
+	Type       string          `json:"type"`
+	Stauts     string          `json:"status"`
+	Ip         string          `json:"ip"`
+	Dependent  Task            `json:"dependent"`
 }

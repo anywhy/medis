838293f240bdfd53a9a4c26d066677b893117186
markyang-_- <yangdaixing168@163.com>
2016-11-01 21:32:13 +0800

    update

--- pkg/sched/config.go
+++ pkg/sched/config.go
@@ -1,9 +1,39 @@
 package sched
 
 type Config struct {
-	Master string
-	Name string
-	User string
+	Master    string
+	Name      string
+	User      string
 	Principal string
-	Zk string
+	Zk        string
+}
+
+func NewConfig(master string, name string, user string, principal string, zk string) *Config {
+	return &Config{
+		Master:    master,
+		Name:      name,
+		User:      user,
+		Principal: principal,
+		Zk:        zk,
+	}
+}
+
+func (c *Config) GetMaster() string  {
+	return c.Master
+}
+
+func (c *Config) GetName() string  {
+	return c.Name
+}
+
+func (c *Config) GetUser() string  {
+	return c.User
+}
+
+func (c *Config) GetPrincipal() string  {
+	return c.Principal
+}
+
+func (c *Config) GetZk() string  {
+	return c.Zk
 }
